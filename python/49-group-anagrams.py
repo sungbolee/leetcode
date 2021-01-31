983d117eb482c1cd57855e0b782ff2939eeb3a2a
sungbolee <sungbobackend@gmail.com>
2021-01-31 23:55:10 +0900

    .

---
+++ python/49-group-anagrams.py
@@ -0,0 +1,11 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()

34949af293d14b3379729d6b5222c0cfbb522bde
sungbolee <sungbobackend@gmail.com>
2021-02-10 23:57:50 +0900

    .

--- python/238-product-of-array-except-self.py
+++ python/238-product-of-array-except-self.py
@@ -6,11 +6,15 @@ class Solution:
         out = []
         p = 1
         for i in range(0, len(nums)):
+            print(i)
             out.append(p)
-            print(p)
             p = p * nums[i]
         p = 1
         for i in range(len(nums) - 1, 0 - 1, -1):
             out[i] = out[i] * p
             p = p * nums[i]
         return out
+
+
+a = Solution()
+print(a.productExceptSelf([1, 2, 3, 4]))
